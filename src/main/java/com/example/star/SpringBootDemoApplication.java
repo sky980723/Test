b9bcc26ca6810a4ff3425f3a426fa98ea4aada12
sky980723 <917968474@qq.com>
2020-08-12 22:35:23 +0800

    增加jar包/修改springboot的启动文件，增加注解

--- src/main/java/com/example/star/SpringBootDemoApplication.java
+++ src/main/java/com/example/star/SpringBootDemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.star;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableSwagger2
+@EnableTransactionManagement
+@MapperScan("com.example.star.dao")
 public class SpringBootDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
-    }
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootDemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
 
 }
