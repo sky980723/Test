b741f4b3fc9a4a75fc0b9856711e75016fd19784
sky980723 <917968474@qq.com>
2020-08-12 22:39:45 +0800

    修改springfox-swagger2版本为2.9.2

--- src/main/java/com/example/star/SpringBootDemoApplication.java
+++ src/main/java/com/example/star/SpringBootDemoApplication.java
@@ -3,7 +3,6 @@ package com.example.star;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,10 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableTransactionManagement
 @MapperScan("com.example.star.dao")
 public class SpringBootDemoApplication {
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootDemoApplication.class);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
